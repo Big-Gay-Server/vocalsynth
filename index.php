114aa7e02ce05466e1e5137192475d86d8192721
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-10 21:31:35 +0000

    testing codespaces woahhh

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Hey hey! Name's Lunar!
 
 I've been working with Vocal Synthesis programs since around 2010! I mostly focus on UTAU projects - I am the voice provider and manager of the UTAU Merisdae, as well as the manager of the Children of Dust UTAU group which contains a bunch of voicebanks voiced by my awesome friends :) I've also dabbled in other VocalSynth programs, such as Vocaloid and DeepVocal - but my focus is definitely UTAU!<br><br>
 
-This site was entirely coded by me by hand, so please forgive any scuff! Currently, it is not mobile friendly.<br><br>
+This site was entirely coded by me by hand, so please forgive any scuff! I am hoping for it to be fully mobile friendly very soon!!<br><br>
 
 Thanks for stopping by!!!
 
