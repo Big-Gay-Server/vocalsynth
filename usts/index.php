5279bcd9a9fdaae7582dd4a154746b819ce1770c
LunarConstruct <lunarconstruct@gmail.com>
2026-04-12 22:39:09 -0400

    lookin good!

--- usts/index.php
+++ usts/index.php
@@ -1,8 +1,8 @@
 <body>
     <header>
-        <h1 dir="auto">LunarConstruct's USTs</h1>
-        <p dir="auto">A list of UTAU USTs made by LunarConstruct. Please credit when using!
-        </p>
+        <h1>LunarConstruct's USTs</h1>
+        <p>A list of UTAU USTs made by LunarConstruct. Please credit when using!</p>
+        <p><strike>also u can't blame me if some of the old ones are bad i am not responsible for 14 year old me's ust making capabilities</strike>
     </header>
     <div>
         <table id="ust-table">
@@ -17,329 +17,43 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Kaleidoscope
-                    </td>
-                    <td>Gomen Nasai ga Ienakute</td>
-                    <td><span>japanese</span></td>
-                    <td>USTx</td>
-                    <td>Nov 17, 2024</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>My
-                        Love is Hellfire</td>
-                    <td>SLAVE.V-V-R</td>
-                    <td><span>japanese</span></td>
-                    <td>USTx</td>
-                    <td>Jan 9, 2022</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Noro</td>
-                    <td>¿? shimon</td>
-                    <td><span>japanese</span></td>
-                    <td>USTx</td>
-                    <td>Dec 28, 2021</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Bakushou
-                    </td>
-                    <td>syudou</td>
-                    <td><span>japanese</span></td>
-                    <td>USTx</td>
-                    <td>Nov 24, 2021</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Utage
-                    </td>
-                    <td>john / TOOBOE</td>
-                    <td><span>japanese</span></td>
-                    <td>USTx</td>
-                    <td>Nov 14, 2021</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Nothing’s
-                        Working Out</td>
-                    <td>meiyo</td>
-                    <td><span>japanese</span></td>
-                    <td>USTx</td>
-                    <td>Nov 7, 2021</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Bakrom
-                    </td>
-                    <td>Hiiragi Kirai</td>
-                    <td><span>japanese</span></td>
-                    <td>USTx</td>
-                    <td>Oct 21, 2021</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Prom
-                        Night</td>
-                    <td>Anamanaguchi</td>
-                    <td><span>english</span></td>
-                    <td>VCCV</td>
-                    <td>Mar 31, 2020</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Inori
-                        no Hana</td>
-                    <td>Torine</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Mar 22, 2019</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>lilac
-                        time</td>
-                    <td>Yukari Hashimoto</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>May 1, 2018</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Tsubomi
-                            Yume Miru Rapsodia ~Alma no Michibiki~</td>
-                    <td>Aya Higashinomiya,Koji Nakagawa,Yuji Masubuchi</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Apr 7, 2018</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Endless
-                        Starlight</td>
-                    <td>Kanae Asaba</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Nov 12, 2017</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Vanilla
-                        Twilight</td>
-                    <td>Owl City</td>
-                    <td><span>english</span></td>
-                    <td>VCCV</td>
-                    <td>Oct 22, 2017</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Kyoumei Sekai no Ontology</td>
-                    <td>Karasuyasabou</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Sep 7, 2017</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Radio
-                        Happy</td>
-                    <td>Taku Inoue</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Sep 6, 2017</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Amber
-                        Sunset</td>
-                    <td>Kevin Mitsunaga</td>
-                    <td><span>japanese</span></td>
-                    <td>CVVC</td>
-                    <td>Sep 4, 2016</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>ROTARY
-                        DIAL</td>
-                    <td>GHOST</td>
-                    <td><span>english</span></td>
-                    <td>VCCV</td>
-                    <td>May 1, 2016</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>True
-                        Colors</td>
-                    <td>Zedd</td>
-                    <td><span>english</span></td>
-                    <td>VCCV</td>
-                    <td>Mar 2, 2016</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Daisy
-                    </td>
-                    <td>Zedd</td>
-                    <td><span>english</span></td>
-                    <td>VCCV</td>
-                    <td>Feb 29, 2016</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Pay no
-                        Mind</td>
-                    <td>Madeon,Madeon</td>
-                    <td><span>english</span></td>
-                    <td>VCCV</td>
-                    <td>Jan 17, 2016</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Ta Fete
-                    </td>
-                    <td>Stromae</td>
-                    <td><span>french</span></td>
-                    <td>CVVC+</td>
-                    <td>Aug 16, 2015</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Elastic
-                        Heart</td>
-                    <td>Sia</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Aug 16, 2015</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Arrest
-                        Rose</td>
-                    <td>natsuP</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Apr 4, 2015</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Titanium
-                    </td>
-                    <td>Sia</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Aug 21, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Chandelier
-                    </td>
-                    <td>Sia</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Aug 21, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Yubikiri
-                        Genman</td>
-                    <td>mili,mili</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Jul 28, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Biohazard
-                    </td>
-                    <td>Crusher-P</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Jul 26, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Shangri-La
-                    </td>
-                    <td>Angela</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Jun 14, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Our
-                        Belief</td>
-                    <td>Circus-P</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Mar 29, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Sirius
-                    </td>
-                    <td>Tenrou</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Mar 27, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Eros
-                        and Apollo</td>
-                    <td>Studio Killers</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Mar 11, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Rainbow
-                        Veins</td>
-                    <td>Owl City</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Feb 20, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Koko
-                        Dake no Hanashi</td>
-                    <td>Chatmonchy,Chatmonchy</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Feb 14, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>How
-                        Deep is Your Love</td>
-                    <td>The Bird and the Bee</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Jan 21, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Let it
-                        Go</td>
-                    <td>Indina Menzel</td>
-                    <td><span>english</span></td>
-                    <td>CVVC</td>
-                    <td>Jan 10, 2014</td>
-                </tr>
-                <tr>
-                    <td>DOWNLOAD</td>
-                    <td>Paradise
-                        Song</td>
-                    <td>yuniP</td>
-                    <td><span>japanese</span></td>
-                    <td>VCV</td>
-                    <td>Dec 9, 2013</td>
-                </tr>
+                <?php
+                require_once '../Spyc.php';
+
+                $ustsDir = glob(__DIR__ . '/downloads/*/', GLOB_ONLYDIR);
+                $sortedData = [];
+
+                foreach ($ustsDir as $ust) {
+                    $infoFile = $ust . 'info.yml';
+
+                    if (file_exists($infoFile)) {
+                        $ustData = Spyc::YAMLLoad($infoFile);
+
+                        if (is_array($ustData) && isset($ustData['name']) && trim($ustData['name']) !== '') {
+                            $ustData['folder'] = basename($ust);
+                            $sortedData[] = $ustData;
+                        }
+                    }
+                }
+
+                if (!empty($sortedData)) {
+                    usort($sortedData, function ($a, $b) {
+                        return strcmp($b['date'] ?? '', $a['date'] ?? '');
+                    });
+
+                    foreach ($sortedData as $data) {
+                        echo '<tr>';
+                        echo '<td><a href="./downloads/' . ($data['folder'] ?? '') . '/' . ($data['dllink'] ?? '') . '"><button id="ustdl">download</button></a></td>';
+                        echo '<td>' . htmlspecialchars($data['name'] ?? 'Unknown') . '</td>';
+                        echo '<td>' . htmlspecialchars($data['artist'] ?? 'Unknown') . '</td>';
+                        echo '<td>' . htmlspecialchars($data['language'] ?? 'N/A') . '</td>';
+                        echo '<td>' . htmlspecialchars($data['format'] ?? 'N/A') . '</td>';
+                        echo '<td>' . htmlspecialchars($data['date'] ?? 'N/A') . '</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>
             </tbody>
-        </table><br /><br />
+        </table>
     </div>
 </body>
