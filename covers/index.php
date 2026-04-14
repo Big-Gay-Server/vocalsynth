e2cd383c7d1d6c02b8a37b6f2fafe10cbb30205a
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-14 21:34:33 +0000

    stylin

--- covers/index.php
+++ covers/index.php
@@ -134,7 +134,7 @@ foreach ($all_covers as $coverdata) {
     // if cover meets below requirements, output a box with the info in it.
     if (!empty($raw_video) && $year >=2016) {
         ?>
-        <div class="coverbox">
+        <a href="<?=$coverpage ?>"><div class="coverbox"></a>
             <?=$video ?> 
             <a href="<?=$coverpage ?>"><h3><?= htmlspecialchars($title) ?></h3></a>
             <h4><?= $vb_display ?></h4> <!-- Removed the outer <a> tag so each name has its own link -->
