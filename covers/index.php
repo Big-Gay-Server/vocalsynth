1fb281a3cbfbe7c6da44dcb29773541ccc1768e9
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-14 20:22:16 +0000

    oh im like DUMB dumb

--- covers/index.php
+++ covers/index.php
@@ -76,7 +76,7 @@ foreach ($all_covers as $coverdata) {
         foreach ($all_vbs as $voicebank) {
             $voicebank_info = Yaml::parseFile($voicebank);
     
-            if (($voicebank_info['name'] ?? '') === $vb) {
+            if (($voicebank_info['vbname'] ?? '') === $vb) {
                 $folder_name = basename(dirname($voicebank));
                 $vb_link = '/voicebanks/' . $folder_name;
                 break;
