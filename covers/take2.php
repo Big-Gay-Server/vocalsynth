7e5e36f8644b6a4d9ba92d393cd7c753acd8ca7e
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-14 13:53:38 +0000

    okay i think i figure it out

--- covers/take2.php
+++ covers/take2.php
@@ -41,7 +41,9 @@ foreach ($folders as $song) {
 
         $title = $multicover ? $song . " (" . $year . ")" : $song;
 
-        $lyric = $coverdata['lyric for desc'] ? $coverdata['lyric for desc']: '';
+        $lyric_data = $coverdata['lyric for desc'] ?? '';
+        $lyric = is_array($lyric_data) ? implode(', ', $lyric_data): $lyric_data;
+        
         $comment = $coverdata['comments'] ? $coverdata['comments'] : '';
         $desc = "\"" . $lyric . "\"" . "<br>" . $comment;
 
