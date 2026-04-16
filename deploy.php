f8647fc5bc512dd8d3e18065ee0d6387d1750f61
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-16 15:06:35 +0000

    fix deploy with secret

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
-// // Simple verification (optional but recommended) - commented out for now until i figure this out
-// $secret = 'your_shared_secret';
-// $signature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
+// Simple verification (optional but recommended) - commented out for now until i figure this out
+$secret = 'JUdleaAm3uuaRPKzcv7ToJQEW';
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
 
 // Run the git pull command
 $output = shell_exec("cd /mnt/raid5/create/vocalsynth && git pull origin main 2>&1");
