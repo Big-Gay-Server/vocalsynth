0d41e49d54579a3be50f7f3df89ab38627624cf0
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-16 20:54:27 +0000

    2?

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ Thanks for stopping by!!!
 $manager = new CoverManager();
 
 // fetch covers
-$all_covers = $manager->getCovers(['limit' => 1]); 
+$all_covers = $manager->getCovers(['limit' => 2]); 
 
 // display covers!!! so easy
 echo '<h2>Latest Cover</h2>';
