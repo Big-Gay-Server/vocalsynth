1f425daa828d9f5d5426df32292bea7aefec15e8
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-16 15:10:51 +0000

    hmmm

--- deploy.php
+++ deploy.php
@@ -5,5 +5,5 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
 
 // Run the git pull command
 $output = shell_exec("cd /mnt/raid5/create/vocalsynth && git pull origin main 2>&1");
-echo "<pre>$output</pre>";
-?>
+file_put_contents('deploy_log.txt', date('Y-m-d H:i:s') . "\n" . $output . "\n---\n", FILE_APPEND);
+echo $output;
