81a0b6320563cab4961ae3308afe4c321dfe4567
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-21 20:55:21 +0000

    dasdasdasdas

--- covers/index.php
+++ covers/index.php
@@ -27,7 +27,7 @@ $cover = $manager->getCovers($specific_filters);
 if ($requested_cover) {
     // Check if the array is NOT empty before accessing index 0
     if (!empty($cover)) {
-        $manager->coverDetails($cover[0]);        
+        $manager->coverDetails($cover);        
         var_dump($requested_cover); 
     } else {
         echo '<h1>Cover not found</h1>';
