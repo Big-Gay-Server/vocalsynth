5fd9b793ad13afec749f788cf9b5f6cbbb0aacbc
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-21 16:41:21 +0000

    bwah

--- news/post.php
+++ news/post.php
@@ -2,31 +2,29 @@
 require_once __DIR__ . '/Parsedown.php';
 $Parsedown = new Parsedown();
 
-// Fix: Since post.php is in /news, just look in the 'posts' subfolder
-$post_dir = __DIR__ . '/posts/'; 
-$file = $_GET['f'] ?? '';
+$post_dir = __DIR__ . '/posts/'; // set folder the posts are in
+$file = $_GET['f'] ?? ''; // get the requested post from url and set it as $file
 
-$path = realpath($post_dir . $file);
+$path = realpath($post_dir . $file); // make the path from the directory and file
 
 // Verify the file exists and is inside the posts folder
 if ($path && strpos($path, $post_dir) === 0 && file_exists($path)) {
-    $markdown = file_get_contents($path);
-    
-    echo "<article class='blog-post' style='max-width: 900px; margin: auto;'>";
-    echo $Parsedown->text($markdown); 
-    echo "</article>";
+    $markdown = file_get_contents($path); // get contents of requested post
+
+?>
+    <article class='blog-post' style='max-width: 900px; margin: auto;'>
+        <?= $Parsedown->text($markdown) ?>
+    </article>
+<?php
+
 } else {
     echo "<h1>Post not found.</h1>";
-    // Debug info to help you see what's wrong:
-    echo "<!-- Script is looking in: " . $post_dir . " for file: " . $file . " -->";
 }
 
 // back to news page
-    echo "<div class='center' style='margin-top: 50px;'>";
-    echo "    <a href='/news' style='text-decoration: none;'>";
-    echo "        <button type='button'>← Back to News</button>";
-    echo "    </a>";
-    echo "</div>";
-
-
 ?>
+<div class='center' style='margin-top: 50px;'>
+    <a href='/news' style='text-decoration: none;'>
+        <button type='button'>← Back to News</button>
+    </a>
+</div>
--- news/posts/utau-day-showcase.md
+++ news/posts/utau-day-showcase.md
@@ -4,7 +4,7 @@ hello hello first post that is not a cover !!
 
 i entered this just for funsies but ended up getting accepted ^^; so please take a look at this!
 
-![merisdae's entry in the utau day 2026 showcase](/news/posts/utau-day-showcase.png)
+![merisdae's entry in the utau day 2026 showcase](/news/posts/images/utau-day-showcase.png)
 
 Merisdae was accepted into the UTAU Day 2026 Voicebank showcase! This is being put on by ExpressiveLabs, who are producing Mikoto Studio ! It's an UTAU-compatible vocal synthesis software, I'm very very interested in it!
 
