96ddfd66696432b088a2539c39fc7cd1670ef2d9
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-22 21:09:14 +0000

    test

--- usts/index.php
+++ usts/index.php
@@ -1,57 +1,50 @@
-<body>
-    <header>
-        <h1>LunarConstruct's USTs</h1>
-        <p>A list of UTAU USTs made by LunarConstruct. Please credit when using!</p>
-        <p><strike>also u can't blame me if some of the old ones are bad i am not responsible for 14 year old me's ust making capabilities</strike>
-    </header>
-    <div>
-        <table id="ust-table">
-            <thead>
-                <tr>
-                    <th>download</th>
-                    <th>song name</th>
-                    <th>original artist</th>
-                    <th>format/lang</th>
-                    <th>date</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/Spyc.php';
-
-                $ustsDir = glob(__DIR__ . '/downloads/*/', GLOB_ONLYDIR);
-                $sortedData = [];
-
-                foreach ($ustsDir as $ust) {
-                    $infoFile = $ust . 'info.yml';
-
-                    if (file_exists($infoFile)) {
-                        $ustData = Spyc::YAMLLoad($infoFile);
-
-                        if (is_array($ustData) && isset($ustData['name']) && trim($ustData['name']) !== '') {
-                            $ustData['folder'] = basename($ust);
-                            $sortedData[] = $ustData;
-                        }
-                    }
-                }
-
-                if (!empty($sortedData)) {
-                    usort($sortedData, function ($a, $b) {
-                        return strcmp($b['date'] ?? '', $a['date'] ?? '');
-                    });
-
-                    foreach ($sortedData as $data) {
-                        echo '<tr>';
-                        echo '<td><a href="./downloads/' . ($data['folder'] ?? '') . '/' . ($data['dllink'] ?? '') . '"><button id="ustdl">download</button></a></td>';
-                        echo '<td>' . htmlspecialchars($data['name'] ?? 'Unknown') . '</td>';
-                        echo '<td>' . htmlspecialchars($data['artist'] ?? 'Unknown') . '</td>';
-                        echo '<td>' . htmlspecialchars($data['language'] ?? 'N/A') . '<br>' . htmlspecialchars($data['format'] ?? 'N/A') . '</td>';
-                        echo '<td>' . htmlspecialchars($data['date'] ?? 'N/A') . '</td>';
-                        echo '</tr>';
-                    }
-                }
-                ?>
-            </tbody>
-        </table>
+<div id="ust-container">
+    <!-- Header Row -->
+    <div class="ust-header">
+        <div>download</div>
+        <div>song name</div>
+        <div>original artist</div>
+        <div>format/lang</div>
+        <div>date</div>
     </div>
-</body>
+
+    <?php 
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/Spyc.php';
+    
+    $ustsDir = glob(__DIR__ . '/downloads/*/', GLOB_ONLYDIR);
+    $sortedData = [];
+
+    foreach ($ustsDir as $ust) {
+        $infoFile = $ust . 'info.yml';
+        if (file_exists($infoFile)) {
+            $ustData = Spyc::YAMLLoad($infoFile);
+            if (is_array($ustData) && isset($ustData['name']) && trim($ustData['name']) !== '') {
+                $ustData['folder'] = basename($ust);
+                $sortedData[] = $ustData;
+            }
+        }
+    }
+
+    if (!empty($sortedData)) {
+        // Your existing sorting logic
+        usort($sortedData, function ($a, $b) {
+            return strcmp($b['date'] ?? '', $a['date'] ?? '');
+        });
+
+        // Loop through the data and echo DIVs instead of TRs
+        foreach ($sortedData as $data) {
+            $dlPath = './downloads/' . ($data['folder'] ?? '') . '/' . ($data['dllink'] ?? '');
+            
+            echo '<div class="ust-row" onclick="window.location=\'' . $dlPath . '\'">';
+                echo '<div class="col-dl"><a href="' . $dlPath . '"><button class="ustdl-btn">download</button></a></div>';
+                echo '<div class="col-name">' . htmlspecialchars($data['name'] ?? 'Unknown') . '</div>';
+                echo '<div class="col-artist">' . htmlspecialchars($data['artist'] ?? 'Unknown') . '</div>';
+                echo '<div class="col-format">' . htmlspecialchars($data['language'] ?? 'N/A') . '<br><small>' . htmlspecialchars($data['format'] ?? 'N/A') . '</small></div>';
+                echo '<div class="col-date">' . htmlspecialchars($data['date'] ?? 'N/A') . '</div>';
+            echo '</div>';
+        }
+    } else {
+        echo '<p>No USTs found.</p>';
+    }
+    ?>
+</div>
