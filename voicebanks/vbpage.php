838d161023765af19f883c9d74a1ae4ddacb0970
Lunar Skye <75494616+lunarconstruct@users.noreply.github.com>
2026-04-27 20:45:37 +0000

    fix!

--- voicebanks/vbpage.php
+++ voicebanks/vbpage.php
@@ -10,7 +10,7 @@ require_once 'index.php';
         <?= $currentvb['vbblurb'] ?>
     </div>
     <div class="column flex50">
-        <img class="columnimg" src="/voicebanks/<?= $urlchara ?>/<?= $currentvb['id'] ?>/key.png">HELLOTEST<br><br>
+        <img class="columnimg" src="/voicebanks/<?= $urlchara ?>/<?= $currentvb['id'] ?>/key.png"><br><br>
         <center>
             Key Art:
             <a href="<?= $currentvb['keyartistlink'] ?>">
